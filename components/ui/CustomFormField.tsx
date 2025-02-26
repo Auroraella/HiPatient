446881f03e3f984862365c40718e8b9a25df72ad
Auroraella <banmaihn05@gmail.com>
2025-02-26 23:42:06 +1030

    edit

--- components/ui/CustomFormField.tsx
+++ components/ui/CustomFormField.tsx
@@ -1,80 +1,106 @@
-'use client'
+"use client";
 
 import {
-    FormControl,
-    FormDescription,
-    FormField,
-    FormItem,
-    FormLabel,
-    FormMessage,
-} from "@/components/ui/form"
-import { Input } from "@/components/ui/input"
-import { Control } from "react-hook-form"
-import { FormFieldType } from "../forms/PatientForm"
-import React from "react"
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Control } from "react-hook-form";
+import Image from "next/image";
+import { FormFieldType } from "../forms/PatientForm";
+import React from "react";
 interface CustomProps {
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    control: Control<any>,
-    fieldType: FormFieldType,
-    name: string,
-    label?: string,
-    placeholder?: string,
-    iconSrc?: string,
-    iconAlt?: string,
-    disabled?: boolean,
-    dateFormat?: string,
-    showTimeSelect?: boolean,
-    children?: React.ReactNode,
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    renderSkeleton?: (field: any) => React.ReactNode,
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  control: Control<any>;
+  fieldType: FormFieldType;
+  name: string;
+  label?: string;
+  placeholder?: string;
+  iconSrc?: string;
+  iconAlt?: string;
+  disabled?: boolean;
+  dateFormat?: string;
+  showTimeSelect?: boolean;
+  children?: React.ReactNode;
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  renderSkeleton?: (field: any) => React.ReactNode;
 }
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars, @typescript-eslint/no-explicit-any
 const RenderField = ({ field, props }: { field: any; props: CustomProps }) => {
-    return (
-        <Input
-            type="text"
-            placeholder="John Doe"
-        />
-    )
-}
+  const { fieldType, iconSrc, iconAlt, placeholder } = props;
 
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
+  switch (fieldType) {
+    case FormFieldType.INPUT:
+      return (
+        <div className="flex rounded-md border border-dark-500 bg-dark-400">
+          {iconSrc && (
+            <Image
+              src={iconSrc}
+              height={24}
+              width={24}
+              alt={iconAlt || "icon"}
+              className="ml-2"
+            />
+          )}
+
+          <FormControl>
+            <Input
+              placeholder={placeholder}
+              {...field}
+              className="shad-input border-0"
+            />
+          </FormControl>
+        </div>
+      );
+
+      break;
+
+    default:
+      break;
+  }
+};
+
 const CustomFormField = (props: CustomProps) => {
-    const { control, fieldType, name, label } = props;
+  const { control, fieldType, name, label } = props;
 
-    return (
-        <><FormField
-            control={control}
-            name={name}
-            // eslint-disable-next-line @typescript-eslint/no-unused-vars
-            render={({ field }) => (
-                <FormItem className="flex-1">
-                    {fieldType !== FormFieldType.CHECKBOX && label && (
-                        <FormLabel>{label}</FormLabel>
-                    )}
+  return (
+    <>
+      <FormField
+        control={control}
+        name={name}
+        render={({ field }) => (
+          <FormItem className="flex-1">
+            {fieldType !== FormFieldType.CHECKBOX && label && (
+              <FormLabel>{label}</FormLabel>
+            )}
 
-                    <RenderField field={field} props={props} />
+            <RenderField field={field} props={props} />
 
-                    <FormMessage className="shad-error" />
+            <FormMessage className="shad-error" />
+          </FormItem>
+        )}
+      />
+      {/* <FormField
+        control={control}
+        name="username"
+        render={({ field }) => (
+          <FormItem>
+            <FormLabel>Username</FormLabel>
+            <FormControl>
+              <Input placeholder="shadcn" {...field} />
+            </FormControl>
+            <FormDescription>This is your public display name.</FormDescription>
+            <FormMessage />
+          </FormItem>
+        )}
+      /> */}
+    </>
+  );
+};
 
-                </FormItem>
-            )} /><FormField
-                control={control}
-                name="username"
-                render={({ field }) => (
-                    <FormItem>
-                        <FormLabel>Username</FormLabel>
-                        <FormControl>
-                            <Input placeholder="shadcn" {...field} />
-                        </FormControl>
-                        <FormDescription>
-                            This is your public display name.
-                        </FormDescription>
-                        <FormMessage />
-                    </FormItem>
-                )} /></>
-    )
-}
-
-export default CustomFormField
+export default CustomFormField;
